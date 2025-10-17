01d45c8fdbefa2d9615c0c74247f7dccd719968f
Melek Huseynova <rahimagasimzada@gmail.com>
2025-10-17 14:52:52 +0400

    hello1 eledim hello

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let ul = document.querySelector("ul")
 
 omg.addEventListener("click", () => {
     let li = document.createElement("li");
-    li.textContent = "Hello1";
+    li.textContent = "Hello";
     ul.appendChild(li);
 });
 
